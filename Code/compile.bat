3f760b5b9b1c52be09f1db6381ed361740611359
dani26YT <98554456+dani26YT@users.noreply.github.com>
2023-07-04 23:34:49 +0200

    Add files via upload

---
+++ Code/compile.bat
@@ -0,0 +1,27 @@
+color a
+csc 1.c#
+echo 1.c# compilato
+pause
+csc 2.c#
+echo 2.c# compilato
+pause
+csc 3.c#
+echo 3.c# compilato
+pause
+csc 4.c#
+echo 4.c# compilato
+pause
+csc 5.c#
+echo 5.c# compilato
+pause
+csc 6.c#
+echo 6.c# compilato
+pause
+csc 7.c#
+echo 7.c# compilato
+pause
+csc 8.c#
+echo 8.c# compilato
+pause
+echo i file sono stati compilati correttamete
+pause
---
+++ FindTheNumber.bat
@@ -0,0 +1,24 @@
+@ echo off
+echo Benvenuto in FindTheNumber (By @Dani26_yt on Instagrm; @Dani26175 on YouTube)
+echo Quando finisci un livello premi invio
+pause
+echo Apertura del primo livello, quando hai finito premi un tasto per passsare al prossimo livello
+cd Altro
+echo Apertura del livello 1 (il primo)
+1.exe
+echo Apertura del livello 2
+2.exe
+echo Apertura del livello 3
+3.exe
+echo Apertura del livello 4
+4.exe
+echo Apertura del livello 5
+5.exe
+echo Apertura del livello 6
+6.exe
+echo Apertura del livello 7
+7.exe
+echo Apertura del livello 8 (l'ultimo)
+8.exe
+echo hai terminato il gioco
+pause
